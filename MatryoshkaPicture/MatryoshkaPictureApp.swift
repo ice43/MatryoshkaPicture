fa130b05848be37f6fa3cfb53a860f5d5c09e9f8
bowski43 <43mngt@gmail.com>
2024-03-18 16:34:49 +0300

    Initial Commit

---
+++ MatryoshkaPicture/MatryoshkaPictureApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatryoshkaPictureApp.swift
+//  MatryoshkaPicture
+//
+//  Created by Serge Broski on 3/18/24.
+//
+
+import SwiftUI
+
+@main
+struct MatryoshkaPictureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
