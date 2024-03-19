ea44a660bde3dba79e48b2fe1799649db973c5c5
bowski43 <43mngt@gmail.com>
2024-03-19 14:21:20 +0300

    final realization in one view

--- MatryoshkaPicture/ContentView.swift
+++ MatryoshkaPicture/ContentView.swift
@@ -8,8 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var start = true
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            FieldView()
+                .frame(width: 600, height: 500)
+                .offset(y: 200)
+            
+            MatryoshkaView()
+                .offset(y: start ? -60 : 0)
+//                .animation(.spring(
+//                    response: 3,
+//                        dampingFraction: 0,
+//                        blendDuration: 0
+//                 ).repeatForever(autoreverses: true))
+            
+                .animation(.interpolatingSpring(
+                    mass: 1,
+                    stiffness: 600,
+                    damping: 10,
+                    initialVelocity: 0
+                ).repeatForever(), value: start)
+        }
+        .onAppear {
+            start.toggle()
+        }
     }
 }
 
